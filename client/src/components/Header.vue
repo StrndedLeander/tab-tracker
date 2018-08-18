e6d56d9e3f8c2da4a3bb1c79620af307836e0a6a
StrndedLeander <strnded.vandiepen@gmail.com>
2018-08-18 17:29:57 +0200

    nearly up to date

---
+++ client/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<v-toolbar fixed class="cyan" dark>
+  <v-toolbar-title>Tab Tracker</v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-toolbar-items class="hidden-sm-and-down">
+    <v-btn flat><router-link class="links" to="/login">Sign In</router-link></v-btn>
+    <v-btn flat><router-link class="links" to="/register">Sign Up</router-link></v-btn>
+  </v-toolbar-items>
+</v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+.links{
+  text-decoration: none;
+  color: white;
+}
+</style>
